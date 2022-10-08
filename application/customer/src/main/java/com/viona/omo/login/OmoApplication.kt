7b15fbecdfeec6325aa981c4d30d1879fdc4bc47
Viona <viona.maryati@stockbit.com>
2022-10-08 07:14:38 +0700

    Update: adjust dependencies

--- application/customer/src/main/java/com/viona/omo/login/OmoApplication.kt
+++ application/customer/src/main/java/com/viona/omo/login/OmoApplication.kt
@@ -3,15 +3,15 @@ package com.viona.omo.login
 import android.app.Application
 import com.viona.omo.login.di.OmoModule
 import org.koin.core.context.startKoin
-import org.koin.ksp.generated.module
+//import org.koin.ksp.generated.module
 
 
 class OmoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            modules(OmoModule().module)
-        }
+//        startKoin {
+//            modules(OmoModule().module)
+//        }
     }
 }
